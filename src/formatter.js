e80aebf96e751770bed1467a5af48e38d9bb770e
jonathan.essex <jonathan.essex@misys.com>
2017-10-22 03:14:10 -0400

    update to latest abstract-query and fix some errors

--- src/formatter.js
+++ src/formatter.js
@@ -1,38 +1,76 @@
 const { Query } = require('abstract-query');
+const { Stream } = require('iterator-plumbing');
 
 
 /** private */
-function printDimension(context, name) {
-	return name;
+function printDimension(context,name) {
+	if (!context || !context.dimension) return name;
+	return printDimension(context.context, context.dimension) + "." + name;
 }
 
 
 
-/** private */
-function toOperator(operator, value) {
-	switch(operator) {
-		case '>'  : return { $gt: value };
-		case '<'  : return { $lt: value };
-		case '>=' : return { $gte: value };
-		case '<=' : return { $lte: value };
-		case '='  : return value;
-	}	
-}
 
-/** Format a Query as a MongoDB Query */
+/** Format a Query as a MongoDB Query 
+*
+* Use dimension: { $elemMatch: { $eq: "value"} } for fields in array
+*
+* Use dimension: { $elemMatch: { d1: "value", d2: "value"} } for matching array of subdocuments
+*
+* Use { "dimension.d1": "value" } for matching fields in straight subdocument (no array)
+*
+*/
 class Formatter {
 
-	/** Create an and expression 
+	/** private */
+	toOperator(operator, value, short_equals = true) {
+		switch(operator) {
+			case '>'  : return { $gt: value };
+			case '<'  : return { $lt: value };
+			case '>=' : return { $gte: value };
+			case '<=' : return { $lte: value };
+			case '='  : return short_equals ? value : { $eq: value };
+			case 'has' : return { $elemMatch: value };
+			case 'hasAll' : return { $elemMatch: value };
+			case 'match': return value;
+		}	
+		throw new RangeError("unknown operator: " + operator)
+	}
+
+	/** Create an and expression.
+	*
+	* Use short form ( { <dimension> : { <oper> : value }, ...} ) if all items in array provided are simple
+	* criteria (e.g. { <dimension> : { <oper> : value } } ) and all dimension values are unique. Otherwise
+	* revert to long form { $and: [ { <dimension> : { <oper> : value } }, ... ]}.
+	*
 	* @param ands {Array} A chain of and operations
-	* @returns {string} A string representing the chain of and operations.
+	* @returns {Object} An object representing the chain of and operations.
 	*/
     andExpr(...ands) { 
-    	return { $and: ands }
+    	let result = {};
+
+    	// Build a short-form object but set to null if we find an item in 'ands' that does not meet requirements.
+    	const reduction = (accumulator, item) => {
+    		let keys = Object.keys(item);
+    		if (keys.length > 1) return null;
+    		let dimension = keys[0];
+    		if (dimension[0] === "$") return null;
+    		if (result[dimension]) return null;
+    		accumulator[dimension] = item[dimension];
+    		return accumulator;
+    	}
+
+    	// Break out of reduce if we find a null 
+    	const condition = (accumulator) => accumulator;
+
+    	result = Stream.from(ands).reduce(reduction,result,condition);
+
+    	return result || { $and: ands };
     }
 
 	/** Create an or expression 
 	* @param ors {Array} A chain of or operations
-	* @returns {string} A string representing the chain of or operations.
+	* @returns {Object} An object representing the chain of or operations.
 	*/
     orExpr(...ors) { 
     	return { $or: ors }
@@ -43,13 +81,19 @@ class Formatter {
 	* @param operator {string} name of operation
 	* @param value {Object} parameter for operation
 	* @param context {Context} context (used in subqueries)
-	* @returns {string} A string representing the operation
+	* @returns {Object} A object representing the operation
 	*/
     operExpr(dimension, operator, value, context) {
-    	if (operator === 'contains')
-    		return { [dimension] : { $elemMatch: value } };
+
+    	let property = printDimension(context,dimension);
+
+    	if (operator === 'match') return value;
+
+    	if (dimension === null) // inside array 'has' or 'hasAll' clause
+    		return this.toOperator(operator, value, false)
     	else	
-    		return { [dimension] : toOperator(operator, value) }; 
+    		return { [property] : this.toOperator(operator, value) }; 
+
     }
 
     /** Get a formatter instance */
--- test/formatter.test.js
+++ test/formatter.test.js
@@ -12,6 +12,11 @@ describe('Range', () => {
     	expect(query).to.deep.equal({$and: [{ "a": "23" }, { $and: [{ "b": { $gte: 2 } }, { "b": { $lt: 4 } }] }]});
     });
 
+    it('formats another simple query', () => { 
+        let query = Query.from({ a: "23", b: [2,]}).toExpression(MONGO);
+        expect(query).to.deep.equal({ "a": "23", "b": { $gte: 2 } });
+    });
+
     it('formats an or query', () => { 
     	let query = Query
     		.from({ a: "23", b: [2,4]})
@@ -26,6 +31,80 @@ describe('Range', () => {
     		.from({ a: "23", b: { c: 4, d: 2}})
     		.toExpression(MONGO);
 
-    	expect(query).to.deep.equal({$and: [{ "a": "23" }, { b: { $elemMatch: { $and: [{ "c": 4 }, { "d": 2 }] } } }]});
+    	expect(query).to.deep.equal({$and: [{ "a": "23"}, { "b.c": 4, "b.d": 2 }] });
     });
+
+    it('formats a query on an array element', () => { 
+
+    	let query = Query
+    		.from({ a: "23", b: { $has : "bongo" } } )
+    		.toExpression(MONGO);
+
+    	expect(query).to.deep.equal({ "a": "23", b: { $elemMatch: { $eq: "bongo" } } });
+    });
+
+    it('formats a query on a multiple array elements', () => { 
+
+    	let query = Query
+    		.from({ a: "23", b: { $hasAll : ["bongo", "bingo"] } } )
+    		.toExpression(MONGO);
+
+    	expect(query).to.deep.equal(
+    		{
+    			$and: [
+    				{ "a": "23" }, 
+    				{ $and: [
+    						{ b: { $elemMatch: { $eq: "bongo" } } }, 
+    						{ b: { $elemMatch: { $eq: "bingo" } } }
+    					]
+    				} 
+    			]
+    		}
+    	);
+    });
+
+    it('formats a query on a array of documents', () => { 
+
+    	let query = Query.from({ a: "23", b: { $has : { drumkit: "bongo" } } } );
+
+    	debug(JSON.stringify(query));
+
+    	let expression = query.toExpression(MONGO);
+
+    	expect(expression).to.deep.equal(
+    		{
+    			"a": "23", 
+    		    b: { $elemMatch: { drumkit: "bongo" } } 
+    		}
+    	);
+    });
+
+    it ('formats example for README.md', () => {
+        let query = Query
+            .from({ a: "23", b: [2,4]})
+            .or({ a: "24", b: [1,16]})
+            .toExpression(MONGO);
+
+        let result = 
+            {"$or":[
+                {"$and":[
+                    {"a":"23"},
+                    {"$and":[
+                        {"b":{"$gte":2}},
+                        {"b":{"$lt":4}}
+                    ]}
+                ]},
+                {"$and":[
+                    {"a":"24"},
+                    {"$and":[
+                        {"b":{"$gte":1}},
+                        {"b":{"$lt":16}}
+                    ]}
+                ]}
+            ]};
+
+        expect(query).to.deep.equal(result);
+
+    });
+
  });
